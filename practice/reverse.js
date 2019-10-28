a023db2db6054eef8009db8d312dbb1e8c513e1d
mr-fool <mr-fool@users.noreply.github.com>
2019-10-27 18:15:35 -0600

    Update reverse.js

--- practice/reverse.js
+++ practice/reverse.js
@@ -5,7 +5,7 @@ var reverse = function(x) {
         rearrange = rearrange.substring(0, x.length - 1);
         rearrange = "-" + rearrange
     }
-    if(rearrange < Math.pow(-2,31) || Math.pow(2,31) - 1 < rearrange) {
+    if(rearrange < Math.pow(-2,31) || rearrange > (Math.pow(2,31) -1 ) ) {
         return 0;
     }
     return rearrange;
