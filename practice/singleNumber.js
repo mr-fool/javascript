d994419f7a1fb15e83e3415106624bf9bc76d735
mr-fool <mr-fool@users.noreply.github.com>
2019-10-29 15:34:37 -0600

    Update singleNumber.js

--- practice/singleNumber.js
+++ practice/singleNumber.js
@@ -6,13 +6,11 @@ var singleNumber = function(nums) {
     var result = nums[0];
     
     var memory1 = {};
-    var memory2 = {};
     
     for (var i=0; i<nums.length; i++) {
         if (memory1[nums[i]]) {
             // We've already seen this number, so move it to the second hash.
             delete memory1[nums[i]];
-            memory2[nums[i]] = 1;
         }
         else {
             // Mark this number as seen.
